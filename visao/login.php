8a5fc2a6522d80e1c759940ad4e49a3eeda97a20
Fillip Mangia <90710689+fillipgms@users.noreply.github.com>
2023-09-21 15:05:58 -0300

    Add files via upload

--- visao/login.php
+++ visao/login.php
@@ -1,5 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION["usuario"])) {
+    $logado = false;
+} else {
+    $logado = true;
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,10 +18,21 @@
 </head>
 <body>
     <header id="header">
-        <a href="/" id="logo">click!</a>
+        <a href="/click/visao" id="logo">click!</a>
         <div>
             <input type="text" name="pesquisa" id="pesquisa">
-            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png">
+            <?php 
+                if ($logado) {
+                    echo '<img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png">';
+                } else {
+                    echo 
+                    '<div class="conta">
+                        <a href="/click/visao/login.php">Entrar</a>
+                        <a href="/click/visao/cadastro.php">Criar conta</a>
+                    </div>';
+                }
+        
+            ?>
         </div>
     </header>
     <main id="formMain">
