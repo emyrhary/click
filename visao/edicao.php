8a86158a45e7111179a519568861384542f513bd
Fillip Mangia <90710689+fillipgms@users.noreply.github.com>
2023-11-30 16:22:24 -0300

    Add files via upload

--- visao/edicao.php
+++ visao/edicao.php
@@ -13,12 +13,6 @@ if (!isset($_SESSION["usuario"])) {
     mysqli_stmt_execute($stmt);
     $selecionado = mysqli_stmt_get_result($stmt);
     $selecionado = mysqli_fetch_assoc($selecionado);
-
-    $novoHeader = "";
-    $novaImagem = "";
-    $novoNome = "";
-    $novousuario = "";
-    $novabio = "";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,69 +20,85 @@ if (!isset($_SESSION["usuario"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>edição! | <?=$selecionado["nome"]?> </title>
-    <link rel="stylesheet" href="../public/css/style.css">
+    <title>edição! | <?=$selecionado["username"]?></title>
     <link rel="stylesheet" href="../public/css/reset.css">
+    <link rel="stylesheet" href="../public/css/edicao.css">
+    <link rel="stylesheet" href="../public/css/header.css">
     <script src="https://kit.fontawesome.com/4c0a49f720.js" crossorigin="anonymous"></script>
     <script src="../public/js/script.js" defer></script>
 </head>
 <body>
     <?php
-        include("../components/header.php")
+        include("../components/header.php");
+
+        if($selecionado["username"] !== $imgPerfil["username"]){
+            header("Location: /click/visao");
+        }
     ?>
-    <main id="perfil">
-    <section id="seuPerfil">
-            <div id="profileInfos">
-                
-                <label id="novoHeader">
-                    <input type="file" style="display: none" id="inputHeader">
-                    <i class="fa-solid fa-pencil"></i>
-                    <img src="../public/imgs/<?=$selecionado["header"]?>" alt="" id="newHeader">
+
+    <main>
+    <form action="/click/processadores/processar-editar-perfil.php" method="post">
+        <section>
+            <label for="updateHeader" class="imageUpdate inputLabel">
+                <img src="../public/imgs/<?=$selecionado["header"]?>" id="profileHeader">
+                <span class="spanLabel center">
+                    <i class="fa-solid fa-pen"></i>
+                </span>
+                <input type="file" accept="image/png, image/gif, image/jpeg" name="updateHeader" id="updateHeader" value="<?=$selecionado["header"]?>" onChange="mudarHeader()">
+            </label>
+        </section>
+        <section>
+            <div>
+                <label for="updateProfilePic" class="imageUpdate inputLabel" >
+                    <img src="../public/imgs/<?=$selecionado["img_perfil"]?>" id="profilePic">
+                    <span class="spanLabel">
+                        <i class="fa-solid fa-pen"></i>
+                    </span>
+                    <input type="file" accept="image/png, image/gif, image/jpeg" name="updateProfilePic" id="updateProfilePic" value="<?=$selecionado["img_perfil"]?>" onChange="mudarProfile()">
                 </label>
-                <label id="novaFoto">
-                    <input type="file" style="display: none" id="inputPic">
-                    <i class="fa-solid fa-pencil"></i>
-                    <img src=
-                        <?php
-                            echo "../public/imgs/". $selecionado["img_perfil"]; 
-                        ?>
-                    >
-                </label>
-                   
             </div>
-            <div id="usuario">
-                <div>
-                    <label for="nome">Nome:</label>
-                    <input type="text" name="nome" value="<?=$selecionado["nome"]?>">
-                </div>
-                <div>
-                    <label for="username">username:</label>
-                    <input type="text" name="username" value="<?=$selecionado["username"]?>">
-                </div>
-                <div>
-                    <label for="biografia">Biorafia:</label>
-                    <textarea name="biografia"><?=$selecionado["bio"]?></textarea>
-                </div>
-                
+            <div>
+                    <div class="inputsFlex">
+                        <div class="inputContainer">
+                            <label for="updateNome">Nome:</label>
+                            <input type="text" name="updateNome" id="updateNome" value="<?=$selecionado["nome"]?>">
+                        </div>
+                        <div class="inputContainer">
+                            <label for="updateUsername">Username:</label>
+                            <input type="text" name="updateUsername" id="updateUsername" value="<?=$selecionado["username"]?>">
+                        </div>
+                    </div>
+                    <div class="inputContainer">
+                        <label for="updateBio">Biografia:</label>
+                        <textarea name="updateBio" id="updateBio"><?=$selecionado["bio"]?></textarea>
+                    </div>
+                    <input type="text" id="notshow" name="email" value="<?=$selecionado["email"]?>">
+                    <input type="submit" value="atualizar dados">
+                </form>
             </div>
         </section>
     </main>
-
     <script>
-        
-        const headerInput = document.getElementById("inputHeader");
-        headerInput.addEventListener('change', function(evt) {
-            let header = document.getElementById("newHeader");
-            var r = new FileReader();
-            r.onload = function() {
-                header.src = r.result;
+        const updateProfilePic = document.getElementById("updateProfilePic");
+        const profilePic = document.getElementById("profilePic"); 
+        const updateHeader = document.getElementById("updateHeader");
+        const header = document.getElementById("profileHeader");
+
+        function mudarHeader () {
+            const [arquivo] = updateHeader.files;
+
+            if (arquivo) {
+                header.src = URL.createObjectURL(arquivo);
             }
-            r.readAsDataURL(evt.target.files[0]);
-            document.getElementById("newHeader").src = headerInput.value;
-        })
+        }
 
-        
+        function mudarProfile () {
+            const [arquivo] = updateProfilePic.files;
 
+            if (arquivo) {
+                profilePic.src = URL.createObjectURL(arquivo);
+            }
+        }
     </script>
 </body>
 </html>
